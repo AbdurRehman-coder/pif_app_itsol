2c522993f6bf623c513714c7f030b7d163990ee9
Kapil M <kapil@eventtan.com>
2023-05-23 12:21:23 +0530

    - Fixed Scroll issue while keyboard is open

--- lib/ui/booking/popup/add_visitor_popup.dart
+++ lib/ui/booking/popup/add_visitor_popup.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pif_flutter/generated/l10n.dart';
@@ -27,228 +26,224 @@ void addVisitorBottomSheet({
     ),
     context: context,
     builder: (BuildContext context) {
-      return KeyboardVisibilityBuilder(
-        builder: (context, isKeyboardVisible) {
-          return Container(
-            padding: EdgeInsets.only(
-              left: 16.w,
-              right: 16.w,
-              top: 24.h,
-              bottom: isKeyboardVisible ? 220.h : 40.h,
-            ),
-            child: Padding(
-              padding: MediaQuery.of(context).viewInsets,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      return Container(
+        padding: EdgeInsets.only(
+          left: 16.w,
+          right: 16.w,
+          top: 24.h,
+          bottom: 40.h,
+        ),
+        child: Padding(
+          padding: MediaQuery.of(context).viewInsets,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          S.of(context).inviteVisitor,
-                          style: Style.commonTextStyle(
-                            color: textColor,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          padding: EdgeInsets.zero,
-                          onPressed: AppRouter.pop,
-                          icon: SvgPicture.asset(
-                            Assets.cancelBtn,
-                          ),
-                        )
-                      ],
-                    ),
                     Text(
-                      S.of(context).visitorOutsideCoBuilder,
+                      S.of(context).inviteVisitor,
                       style: Style.commonTextStyle(
-                        color: silverTextColor,
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w400,
+                        color: textColor,
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                    SizedBox(
-                      height: 16.h,
-                    ),
-                    MarginWidget(
-                      horizontal: -16,
-                      child: Container(
-                        height: 1.h,
-                        color: lineColor,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 24.h,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: notifier.visitorFirstNameController,
-                            decoration: InputDecoration(
-                              isDense: true,
-                              fillColor: lightGrayBgColor,
-                              filled: true,
-                              disabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                              ),
-                              labelStyle: Style.commonTextStyle(
-                                color: grayTextColor,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.w400,
-                              ),
-                              constraints: BoxConstraints(maxHeight: 50.h),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                              ),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                              ),
-                              labelText: S.of(context).firstName,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 16.w,
-                        ),
-                        Expanded(
-                          child: TextField(
-                            controller: notifier.visitorLastNameController,
-                            decoration: InputDecoration(
-                              isDense: true,
-                              fillColor: lightGrayBgColor,
-                              filled: true,
-                              disabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                              ),
-                              labelStyle: Style.commonTextStyle(
-                                color: grayTextColor,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.w400,
-                              ),
-                              constraints: BoxConstraints(maxHeight: 50.h),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                              ),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: borderColor, width: 1.w),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6.r),
-                                ),
-                              ),
-                              labelText: S.of(context).lastName,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 24.h,
-                    ),
-                    TextField(
-                      controller: notifier.visitorEmailController,
-                      decoration: InputDecoration(
-                        isDense: true,
-                        fillColor: lightGrayBgColor,
-                        filled: true,
-                        prefixIcon: SvgPicture.asset(
-                          Assets.email,
-                          fit: BoxFit.scaleDown,
-                        ),
-                        disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(6.r),
-                          ),
-                          borderSide: BorderSide(color: borderColor, width: 1.w),
-                        ),
-                        labelStyle: Style.commonTextStyle(
-                          color: grayTextColor,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        constraints: BoxConstraints(maxHeight: 50.h),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(6.r),
-                          ),
-                          borderSide: BorderSide(color: borderColor, width: 1.w),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: borderColor, width: 1.w),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(6.r),
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: borderColor, width: 1.w),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(6.r),
-                          ),
-                        ),
-                        labelText: S.of(context).visitorEmail,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15.h,
-                    ),
-                    Text(
-                      provider.errorMessage,
-                      style: Style.commonTextStyle(
-                        color: redColor,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        notifier.inviteAsync();
-                      },
-                      style: Style.primaryButtonStyle(
-                        context: context,
-                      ),
-                      child: Text(
-                        S.of(context).invite,
+                    const Spacer(),
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: AppRouter.pop,
+                      icon: SvgPicture.asset(
+                        Assets.cancelBtn,
                       ),
                     )
                   ],
                 ),
-              ),
+                Text(
+                  S.of(context).visitorOutsideCoBuilder,
+                  style: Style.commonTextStyle(
+                    color: silverTextColor,
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                MarginWidget(
+                  horizontal: -16,
+                  child: Container(
+                    height: 1.h,
+                    color: lineColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 24.h,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: notifier.visitorFirstNameController,
+                        decoration: InputDecoration(
+                          isDense: true,
+                          fillColor: lightGrayBgColor,
+                          filled: true,
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                          ),
+                          labelStyle: Style.commonTextStyle(
+                            color: grayTextColor,
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          constraints: BoxConstraints(maxHeight: 50.h),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                          ),
+                          labelText: S.of(context).firstName,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16.w,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: notifier.visitorLastNameController,
+                        decoration: InputDecoration(
+                          isDense: true,
+                          fillColor: lightGrayBgColor,
+                          filled: true,
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                          ),
+                          labelStyle: Style.commonTextStyle(
+                            color: grayTextColor,
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          constraints: BoxConstraints(maxHeight: 50.h),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor, width: 1.w),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6.r),
+                            ),
+                          ),
+                          labelText: S.of(context).lastName,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24.h,
+                ),
+                TextField(
+                  controller: notifier.visitorEmailController,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    fillColor: lightGrayBgColor,
+                    filled: true,
+                    prefixIcon: SvgPicture.asset(
+                      Assets.email,
+                      fit: BoxFit.scaleDown,
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.r),
+                      ),
+                      borderSide: BorderSide(color: borderColor, width: 1.w),
+                    ),
+                    labelStyle: Style.commonTextStyle(
+                      color: grayTextColor,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    constraints: BoxConstraints(maxHeight: 50.h),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.r),
+                      ),
+                      borderSide: BorderSide(color: borderColor, width: 1.w),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: borderColor, width: 1.w),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.r),
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: borderColor, width: 1.w),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.r),
+                      ),
+                    ),
+                    labelText: S.of(context).visitorEmail,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Text(
+                  provider.errorMessage,
+                  style: Style.commonTextStyle(
+                    color: redColor,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    notifier.inviteAsync();
+                  },
+                  style: Style.primaryButtonStyle(
+                    context: context,
+                  ),
+                  child: Text(
+                    S.of(context).invite,
+                  ),
+                )
+              ],
             ),
-          );
-        },
+          ),
+        ),
       );
     },
   );
