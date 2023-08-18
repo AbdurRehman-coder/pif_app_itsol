14d8bbfe21d75cb4c33ac9099ebed40baff60ddd
Obaida Alqassem <oalqassem@appswave.io>
2023-08-18 11:45:55 +0000

    Merged in bugfix/PCBP2-1462-qamobilevisits-date-picker-ap2 (pull request #150)
    Fix issue in date
    
    * Fix issue in date

--- lib/ui/visit/invite_edit_visit/invite_edit_visit_page.dart
+++ lib/ui/visit/invite_edit_visit/invite_edit_visit_page.dart
@@ -194,7 +194,7 @@ class _InviteVisitorPageState extends ConsumerState<InviteEditVisitPage> {
                         Visibility(
                           visible: provider.isOpenStartDatePicker,
                           child: Padding(
-                            padding: EdgeInsets.only(top: 50.h),
+                            padding: EdgeInsets.only(top: 70.h),
                             child: DatePickerWidget(
                               selectedDate: provider.startDate,
                               onConfirm: notifier.updateStartDate,
@@ -205,7 +205,7 @@ class _InviteVisitorPageState extends ConsumerState<InviteEditVisitPage> {
                         Visibility(
                           visible: provider.isOpenStartTimePicker,
                           child: Padding(
-                            padding: EdgeInsets.only(top: 100.h),
+                            padding: EdgeInsets.only(top: 120.h),
                             child: InkWell(
                               onTap: () {},
                               child: TimePickerPopup(
@@ -225,7 +225,7 @@ class _InviteVisitorPageState extends ConsumerState<InviteEditVisitPage> {
                         Visibility(
                           visible: provider.isOpenEndDatePicker,
                           child: Padding(
-                            padding: EdgeInsets.only(top: 100.h, left: 12.w),
+                            padding: EdgeInsets.only(top: 130.h, left: 12.w),
                             child: DatePickerWidget(
                               selectedDate: provider.endDate,
                               onConfirm: (dateTime) => notifier.updateEndDate(
@@ -239,7 +239,7 @@ class _InviteVisitorPageState extends ConsumerState<InviteEditVisitPage> {
                         Visibility(
                           visible: provider.isOpenEndTimePicker,
                           child: Padding(
-                            padding: EdgeInsets.only(top: 165.h),
+                            padding: EdgeInsets.only(top: 190.h),
                             child: InkWell(
                               onTap: () {},
                               child: TimePickerPopup(
