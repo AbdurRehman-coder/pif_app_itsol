4b8fa2966279885b279dc67349a13c8e3299ab1b
Abdur Rehman <abdul.rahman@appswave.ae>
2023-08-21 07:18:34 +0000

    Merged in PCBP2-1452-rename-terms-of-service-to-be (pull request #163)
    PCBP2-1452 rename terms of service to be
    
    * hide dark mode, FAQs, Language and add redirect for terms & condition [Menu/drawer page].
    
    * Merge branch 'PCBP2-1452-rename-terms-of-service-to-be' of https://bitbucket.org/appswaveio/pif-mobile-app into PCBP2-1452-rename-terms-of-service-to-be
    
    # Conflicts:
    #	lib/ui/side_menu/side_menu_page.dart
    
    * Hide the theme buttons, and add shimmer for terms and condition file.
    
    * max meeting duration time exceeds dynamically.
    
    * Merged main into this branch.
    
    * change the shimmer loading with app default loading on terms and NDU screen.
    
    
    Approved-by: Thaer Abu-Hureira

--- lib/ui/on_boarding/fill_information/privacy_screen.dart
+++ lib/ui/on_boarding/fill_information/privacy_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pif_flutter/common/index.dart';
 import 'package:pif_flutter/common/shared/widget/custom_app_bar.dart';
+import 'package:pif_flutter/common/shared/widget/custom_lottie_widget.dart';
 import 'package:pif_flutter/ui/on_boarding/fill_information/provider/fill_information_provider.dart';
-import 'package:pif_flutter/ui/on_boarding/fill_information/widget/terms_condition_shimmer.dart';
 
 class PrivacyScreen extends ConsumerWidget {
   const PrivacyScreen({required this.fromNda, super.key});
@@ -24,14 +24,23 @@ class PrivacyScreen extends ConsumerWidget {
         .firstOrNull
         ?.contentFieldValue
         ?.data;
-    print('html text:: $htmlTextNDA $htmlTextTerms');
     return Scaffold(
       backgroundColor: lightGrayBgColor,
       appBar: CustomAppBar(
         title: fromNda ? S.current.NDU : S.current.termsOfService,
       ),
       body: htmlTextNDA == null && htmlTextTerms == null
-          ? const TermsConditionShimmerWidget()
+          ? Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.transparent,
+              child: const Center(
+                child: CustomLottieWidget(
+                  lottieUrl: Assets.loaderLottie,
+                  isAssets: true,
+                ),
+              ),
+            )
           : SingleChildScrollView(
               padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
               child: HtmlWidget(
